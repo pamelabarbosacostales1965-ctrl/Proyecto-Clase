cc298347f989be758539adc8d8f3e08f86312905
Pame <pamelabarbosacostales1965@gmail.com>
2026-04-02 08:59:11 -0500

    Implement calculator.py with basic operations and add unit tests

--- app/calculator.py
+++
@@ -1,5 +0,0 @@
-def sum(a:int, b:int) -> int:
-    return a + b
-
-def resta(a:int, b:int) -> int:
-    return a - b
--- test/test_calculator.py
+++
@@ -1,7 +0,0 @@
-from app.calculator import sum, resta
-
-def test_sum():
-    assert sum(2, 3) == 5
-
-def test_resta():
-    assert resta(5, 3) == 2
